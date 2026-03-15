a89ec6a016d86e15d1487e4be85642fcecd183e7
wolharyde <jide.s.oyewole@gmail.com>
2026-03-15 19:33:57 -0400

    Add initial server.js file

--- backend/server.js
+++ backend/server.js
@@ -5,14 +5,17 @@ const cors = require('cors');
 const todoRoutes = require('./routes/todoRoutes');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
-app.use('/api', todoRoutes);
+app.use('/api/todos', todoRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
