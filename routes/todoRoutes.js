ab2ff2b3c538d1daf5e3508cb2f538e75431cade
wolharyde <jide.s.oyewole@gmail.com>
2026-03-15 19:11:59 -0400

    Add todo routes

--- routes/todoRoutes.js
+++ routes/todoRoutes.js
@@ -81,11 +81,21 @@ router.delete('/todos/:id', async (req, res) => {
 // GET statistics
 router.get('/stats', async (req, res) => {
   try {
-    const completedToday = await Todo.countDocuments({
-      completed: true,
-      completedAt: { $gte: new Date().setHours(0, 0, 0, 0) }
-    });
-    res.json({ completedToday });
+    const stats = await Todo.aggregate([
+      {
+        $match: { completed: true }
+      },
+      {
+        $group: {
+          _id: { $dateToString: { format: "%Y-%m-%d", date: "$completedAt" } },
+          count: { $sum: 1 }
+        }
+      },
+      {
+        $sort: { _id: 1 }
+      }
+    ]);
+    res.json(stats);
   } catch (err) {
     res.status(500).json({ message: err.message });
   }
