f6ae4573df91f028f627c6d10e6e58ff2047b042
wolharyde <jide.s.oyewole@gmail.com>
2026-03-16 12:14:03 -0400

    Add Todo model

--- backend/models/Todo.js
+++ backend/models/Todo.js
@@ -1,13 +1,31 @@
 const mongoose = require('mongoose');
 
 const TodoSchema = new mongoose.Schema({
-  title: { type: String, required: true },
-  description: { type: String },
-  completed: { type: Boolean, default: false },
-  createdAt: { type: Date, default: Date.now },
-  completedAt: { type: Date },
-  notes: { type: String },
-  cost: { type: Number }
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: false,
+  },
+  completed: {
+    type: Boolean,
+    default: false,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  completedAt: {
+    type: Date,
+  },
+  notes: {
+    type: String,
+  },
+  cost: {
+    type: Number,
+  },
 });
 
 module.exports = mongoose.model('Todo', TodoSchema);
