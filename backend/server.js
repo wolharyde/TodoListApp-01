2b244982bd503e402c4fe3215729189d096a2f7b
wolharyde <jide.s.oyewole@gmail.com>
2026-03-16 09:14:10 -0400

    Initialize backend server

--- backend/server.js
+++ backend/server.js
@@ -5,12 +5,12 @@ const cors = require('cors');
 const todoRoutes = require('./routes/todoRoutes');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect(process.env.MONGODB_URI, {
+mongoose.connect('mongodb://localhost/todolistapp', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
