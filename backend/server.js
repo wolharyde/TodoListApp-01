f3f06ccda2aa4970960eea9b79fd8b99aa58ea00
wolharyde <jide.s.oyewole@gmail.com>
2026-03-16 06:32:02 -0400

    Add main server file

--- backend/server.js
+++ backend/server.js
@@ -11,18 +11,17 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-// Connect to MongoDB
 mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
-})
-.then(() => console.log('MongoDB connected'))
-.catch((err) => console.log('MongoDB connection error:', err));
+});
 
-// Routes
-app.use('/api/todos', require('./routes/todos'));
-app.use('/api/stats', require('./routes/stats'));
+const todoRoutes = require('./routes/todos');
+const statsRoutes = require('./routes/stats');
+
+app.use('/api/todos', todoRoutes);
+app.use('/api/stats', statsRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
