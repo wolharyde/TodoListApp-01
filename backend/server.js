ace538594a3d0b32eaabe08c4bb820541c32e260
wolharyde <jide.s.oyewole@gmail.com>
2026-03-16 17:58:43 -0400

    Add backend server.js

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
+require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -8,19 +9,16 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-// MongoDB connection (replace with your actual connection string)
-const uri = "mongodb://localhost:27017/todolistapp";
-mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true });
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 const connection = mongoose.connection;
 connection.once('open', () => {
   console.log("MongoDB database connection established successfully");
 })
 
-// Import routes
 const todosRouter = require('./routes/todos');
 const statsRouter = require('./routes/stats');
 
-// Use routes
 app.use('/api/todos', todosRouter);
 app.use('/api/stats', statsRouter);
 
