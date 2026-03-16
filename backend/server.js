f83e995bfeac221d0f948f9da7211cecacc14628
wolharyde <jide.s.oyewole@gmail.com>
2026-03-16 05:36:13 -0400

    Add main server file

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,9 @@
-
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-const todoRoutes = require('./routes/todoRoutes');
+const dotenv = require('dotenv');
+
+dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -10,9 +11,14 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect('mongodb://localhost/todolistapp', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+});
 
-app.use('/api', todoRoutes);
+const todoRoutes = require('./routes/todoRoutes');
+app.use('/api/todos', todoRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
