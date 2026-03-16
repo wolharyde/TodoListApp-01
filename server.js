b3a460c8612e82e9ac9eaf348a22bc7d9f024b14
wolharyde <jide.s.oyewole@gmail.com>
2026-03-16 03:07:26 -0400

    Add main server file

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect('mongodb://localhost/todolistapp', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 app.use('/api', todoRoutes);
 
