d5bb9ba6d489c985b549231b0c6f9bda9c27c5d2
wolharyde <jide.s.oyewole@gmail.com>
2026-03-16 04:00:04 -0400

    Add todo routes

--- backend/routes/todoRoutes.js
+++ backend/routes/todoRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const Todo = require('../models/Todo');
 
 // GET all todos
-router.get('/', async (req, res) => {
+router.get('/todos', async (req, res) => {
   try {
     const todos = await Todo.find();
     res.json(todos);
@@ -13,7 +13,7 @@ router.get('/', async (req, res) => {
 });
 
 // POST a new todo
-router.post('/', async (req, res) => {
+router.post('/todos', async (req, res) => {
   const todo = new Todo({
     title: req.body.title,
     description: req.body.description,
@@ -28,8 +28,8 @@ router.post('/', async (req, res) => {
   }
 });
 
-// PUT update a todo
-router.put('/:id', async (req, res) => {
+// PUT (update) a todo
+router.put('/todos/:id', async (req, res) => {
   try {
     const todo = await Todo.findById(req.params.id);
     if (todo == null) {
@@ -65,7 +65,7 @@ router.put('/:id', async (req, res) => {
 });
 
 // DELETE a todo
-router.delete('/:id', async (req, res) => {
+router.delete('/todos/:id', async (req, res) => {
   try {
     const todo = await Todo.findById(req.params.id);
     if (todo == null) {
