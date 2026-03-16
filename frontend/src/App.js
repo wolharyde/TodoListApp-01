435d1c395aed94d2d5a8135fb475accad5ef7ec8
wolharyde <jide.s.oyewole@gmail.com>
2026-03-16 09:02:35 -0400

    Add main App component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,59 +1,21 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import TodoList from './components/TodoList';
-import TodoForm from './components/TodoForm';
 import Statistics from './components/Statistics';
 
 function App() {
-  const [todos, setTodos] = useState([]);
-
-  useEffect(() => {
-    fetchTodos();
-  }, []);
-
-  const fetchTodos = async () => {
-    try {
-      const response = await axios.get('http://localhost:3000/api/todos');
-      setTodos(response.data);
-    } catch (error) {
-      console.error('Error fetching todos:', error);
-    }
-  };
-
-  const addTodo = async (todo) => {
-    try {
-      const response = await axios.post('http://localhost:3000/api/todos', todo);
-      setTodos([...todos, response.data]);
-    } catch (error) {
-      console.error('Error adding todo:', error);
-    }
-  };
-
-  const updateTodo = async (id, updatedTodo) => {
-    try {
-      const response = await axios.put(`http://localhost:3000/api/todos/${id}`, updatedTodo);
-      setTodos(todos.map(todo => todo._id === id ? response.data : todo));
-    } catch (error) {
-      console.error('Error updating todo:', error);
-    }
-  };
-
-  const deleteTodo = async (id) => {
-    try {
-      await axios.delete(`http://localhost:3000/api/todos/${id}`);
-      setTodos(todos.filter(todo => todo._id !== id));
-    } catch (error) {
-      console.error('Error deleting todo:', error);
-    }
-  };
-
   return (
-    <div className="App">
-      <h1>TodoListApp-01</h1>
-      <TodoForm addTodo={addTodo} />
-      <TodoList todos={todos} updateTodo={updateTodo} deleteTodo={deleteTodo} />
-      <Statistics />
-    </div>
+    <Router>
+      <div className="App">
+        <header>
+          <h1>TodoListApp-01</h1>
+        </header>
+        <Switch>
+          <Route exact path="/" component={TodoList} />
+          <Route path="/stats" component={Statistics} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
